7c89c260e548d29af30f7f4fb339bd31c129fe46
unknown <ethanbarbar@gmail.com>
2020-10-19 20:00:11 +0300

    Ff

--- lecture_4/loop_example.py
+++ lecture_4/loop_example.py
@@ -1,4 +1,31 @@
-for i in range(1, 10000):
-    print(i)
-    if i == 1000:
-        break
+# for i in range(1, 10000):
+#     print(i)
+#     if i == 1000:
+#         break
+def add_one(num):
+    x = num + 1
+    return x
+
+
+def is_lists_equals(lst1, lst2):
+    if len(lst1) != len(lst2):
+        return False
+    for i in range(len(lst1)):
+        if lst1[i] != lst2[i]:
+            return False
+    return True
+
+
+x = [1, 2, 3]
+y = [3, 1, 2]
+print(is_lists_equals(x, x))
+# a = 2
+# y = add_one(a)
+# a = a + 1
+# print(y)
+
+b = 2
+b = b + 1
+# lst = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+# sum_of_lst = sum(lst)
+# print(sum_of_lst)
