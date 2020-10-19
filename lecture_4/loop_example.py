43815bbb305811fbd1b6f5a96958a3bc3a56b030
unknown <ethanbarbar@gmail.com>
2020-10-19 19:20:42 +0300

    ff

---
+++ lecture_4/loop_example.py
@@ -0,0 +1,4 @@
+for i in range(1, 10000):
+    print(i)
+    if i == 1000:
+        break
---
+++ lecture_4/slicing_example.py
@@ -0,0 +1,30 @@
+lst = [1, 2, 3, 4, 5, 6, 7]
+print(lst[5:-1:1])
+
+s = 'Hello ' + 'my name is Ethan!' + '4'
+a = 'Hello'
+b = 'Bli Bli'
+print(a + b)
+print(s)
+print(s[:10])
+
+#
+lst = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+s = 'Hello my name is Ethan!'
+# 1
+print(lst[5:8])
+# 2
+print(lst[:4])
+# 3
+print(lst[7:])
+# 4
+str_length = len(s)
+print(lst[str_length // 2:])
+
+# 5
+print(s[::-1])
+
+# 6
+print(s[5:1:-1])
+
+print("Hello! \\ World")
