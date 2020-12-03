69f0bc0fa4da29df26e268115b20567b2528d937
Borat <ethanbarbar@gmail.com>
2020-12-03 20:03:32 +0200

    ff

--- lecture_17/client_socket_example.py
+++ lecture_17/client_socket_example.py
@@ -2,7 +2,13 @@ import socket
 
 # create an INET, STREAMing socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print('Created client')
 # now connect to the web server on port 15000 - the normal http port
 s.connect(("127.0.0.1", 15000))
+print('Connected to 127.0.0.1 in port 15000')
 s.send(b'Hello There!')
+print('Sent message')
+msg = s.recv(1024)
+print('Got message from server : {}'.format(msg.decode()))
 s.close()
+print('Closing')
--- lecture_17/server_socket_example.py
+++ lecture_17/server_socket_example.py
@@ -2,14 +2,18 @@ import socket
 
 # create an INET, STREAMing socket
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print('Starting server socket with port {}'.format(15000))
 # bind the socket to a public host, and a well-known port
 serversocket.bind(('127.0.0.1', 15000))
 # become a server socket
 serversocket.listen(5)
+print('Starting to listen')
 (clientsocket, address) = serversocket.accept()
+print('Accepted client')
 while True:
     msg = clientsocket.recv(1024)
     if msg == b'':
         break
     else:
-        print(msg.decode())
+        print('got message : ' + msg.decode())
+        clientsocket.send(b'This is server sending message to client!')
