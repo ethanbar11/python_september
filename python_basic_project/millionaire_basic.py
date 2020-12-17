d575dcc59d46bedd5c0d9898f81afe2c0798b14e
Borat <ethanbarbar@gmail.com>
2020-12-17 20:23:58 +0200

    ff

--- python_basic_project/millionaire_basic.py
+++
@@ -1,8 +0,0 @@
-import random
-
-questions = {'question1': 'answer1', 'question2': 'answer2'}
-questions_lst = list(questions.keys())
-question_number = random.randint(0,len(questions_lst)-1)
-
-random_question=questions_lst[question_number]
-print(random_question)
